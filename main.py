b4690b6b4339ad1e6aa8af7fc72e3537d6ddf954
serlikfacundo07 <facundojavierserlik@gmail.com>
2026-06-24 23:03:42 -0300

    nuevo frontend

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ app = Flask(__name__, static_folder='frontend', template_folder='frontend')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 import sqlite3
 import pandas as pd
